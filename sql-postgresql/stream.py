cc2b0046938044c07eddfe04bff8eb81ba6a1612
ritu-pixel <75141887+ritu-pixel@users.noreply.github.com>
2025-03-25 21:22:18 +0530

    Update stream.py

--- sql-postgresql/stream.py
+++ sql-postgresql/stream.py
@@ -1,20 +1,7 @@
-import streamlit as st
-import psycopg2
-
-# Database Connection
-conn = psycopg2.connect(
+conn = psycopg2.connect( # type: ignore
     dbname="testdb",
     user="ritu",
     password="secret",
-    host="my_postgres_container",
+    host="localhost",
     port="5432"
 )
-cur = conn.cursor()
-
-# Fetch Data
-cur.execute("SELECT * FROM passengers;")
-rows = cur.fetchall()
-
-# Streamlit UI
-st.title("🚆 Passenger Database")
-st.table(rows)
